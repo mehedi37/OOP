b3263e12a7be27157112cd2d38bbad4711beb4ac
Mehedi Hasan Maruf <41261534+mehedi37@users.noreply.github.com>
2022-11-01 02:43:47 -0700

    Add files via upload

---
+++ 1204/Module_4/poplyMorphisom__topic_1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Test {
+ public:
+    int sum(int a) {
+        return a;
+    }
+    int sum(int a, int b) {
+        // cout << "int int type\n";
+        return a+b;
+    }
+    double sum(double a, int b) {
+        return a+b;
+    }
+    double sum(int a, double b) {
+        return a+b;
+    }
+    double sum(double a, double b) {
+        return a+b;
+    }
+};
+
+int main() {
+    Test t;
+    cout << t.sum(10) << endl;
+    cout << t.sum(10, 20) << endl;
+    cout << t.sum(5.7, 20) << endl;
+    cout << t.sum(10, 2.6) << endl;
+    cout << t.sum(10.5, 20.7) << endl;
+}
