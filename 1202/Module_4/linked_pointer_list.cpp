ff5420d601a3c71fec19df3cf89d812ebf5c369b
Mehedi Hasan Maruf <41261534+mehedi37@users.noreply.github.com>
2022-11-07 15:27:51 +0600

    Update linked_pointer_list.cpp

--- 1202/Module_4/linked_pointer_list.cpp
+++ 1202/Module_4/linked_pointer_list.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 class Node{
-    public:
-        int data{0};
-        Node *next = NULL;
+ public:
+    int data{0};
+    Node *next = NULL;
 };
 
 class linked_list {
@@ -26,7 +27,6 @@ class linked_list {
         cout << endl;
         for (int i=0; i < n; i++) {
             p = new Node();
-            // int *x = new int[10];
             cin >> x;
             p -> data = x;
             if (h == NULL) {
@@ -254,12 +254,299 @@ class linked_list {
         cin.ignore();
         cin.ignore();
     }
+
+// For test purpose
+    void mk_circular() {
+        p = h;
+        if (p == NULL) {
+            cout << "List empty" << endl;
+        }
+        while (p->next != NULL) {
+            p = p->next;
+        }
+        p->next = h;
+        cin.ignore();
+    }
+    void mk_liner() {
+        p = h;
+        if (p->next == h) {
+            cout << "List empty" << endl;
+        }
+        while (p->next != h) {
+            p = p->next;
+        }
+        p->next = NULL;
+        cin.ignore();
+    }
+// Not for module, ignore till this line
+
+
+
+/* For Circular Linked List */
+void c_create() {
+        h = NULL;
+        q = NULL;
+        p = NULL;
+        cout << "How many numbers : ";
+        cin >> n;
+        cout << endl;
+        for (int i=0; i < n; i++) {
+            p = new Node();
+            cin >> x;
+            p -> data = x;
+            if (h == NULL) {
+                h = p;
+            } else {
+                if (q != NULL) {
+                    q->next = p;
+                }
+            }
+            q = p;
+        }
+        p->next = h;
+        cout << "\tList created\n";
+        cin.ignore();
+    }
+    void c_display_Data() {
+        p = h;
+        if (p->next == h) {
+            cout << "List empty" << endl;
+        }
+        cout << "h";
+        while (p->next != h) {
+            cout << " -> " << p->data;
+            p = p->next;
+        }
+        cout << " -> " << p->data;
+        cin.ignore();
+    }
+    void c_insert_Data() {
+        c_display_Data();
+        p = h;
+        q = h;
+        cout << "\nEnter index (0 based): ";
+        int idx;
+        cin >> idx;
+        cout << "Enter new number : ";
+        int up_val;
+        cin >> up_val;
+        system("cls");
+        int cnt{0};
+        // handling 0 index insertion
+        if (idx == 0) {
+            Node *add = new Node;
+            add->next = h;
+            add->data = up_val;
+            h = add;
+            cout << "\n\tAdded\n";
+            cin.ignore();
+            cin.ignore();
+            return;
+        }
+        while (p->next != h) {
+            if (cnt == idx) {
+                Node *add = new Node;
+                add->data = up_val;
+                add->next = p;
+                q->next = add;
+                cout << "\n\tAdded\n";
+                cin.ignore();
+                cin.ignore();
+                return;
+            }
+            cnt++;
+            q = p;
+            p = p->next;
+        }
+        cnt++;
+        if (cnt == idx) {
+            Node *add = new Node;
+            add->data = up_val;
+            add->next = p;
+            q->next = add;
+            cout << "\n\tAdded\n";
+            cin.ignore();
+            cin.ignore();
+            return;
+        }
+        cout << "\n\tNot Found\n";
+        cin.ignore();
+        cin.ignore();
+    }
+    void c_delete_Data() {
+        c_display_Data();
+        p = h;
+        q = h;
+        cout << "\nEnter number to delete : ";
+        int pos_val;
+        cin >> pos_val;
+        bool fg{1}, repeat{0};
+        cout << "Want to search all the ? (1 or 0) : ";
+        cin >> repeat;
+        // If the value is at beginning of list
+        while (h->data == pos_val && h->next != h) {
+            h = h->next;
+            fg = 0;
+            cout << "\n\tDeleted\n";
+            // Bug fixed for all same value deletion
+            if (h->next == h) {
+                return;
+            }
+            if (!repeat) return;
+        }
+        system("cls");
+        while (p->next != h) {
+            if (p->data == pos_val) {
+                q->next = p->next;    // Keeping the deleted node (p)'s next location (p.next) to the previous node "q"
+                cout << "\n\tDeleted\n";
+                cin.ignore();
+                fg = 0;
+                if (!repeat) return;
+            }
+            q = p;      // First keeping the last node position into "q"
+            p = p->next;    // Then going to the next node
+        }
+        if (p->data == pos_val) {
+            q->next = p->next;
+            cout << "\n\tDeleted\n";
+            cin.ignore();
+            fg = 0;
+            return;
+        }
+        if (fg) cout << "Not Found\n";
+        cin.ignore();
+        cin.ignore();
+    }
+    void c_split_list() {
+        cout << "How many splits : ";
+        cin >> split_count;
+        int spl[split_count]{0};
+        for (int i{0}; i < split_count; i++) {
+            cin >> spl[i];
+        }
+        p = h;
+        q = h;
+        split[0] = h;       // Keeping the head
+        int cnt{0}, idx{1};
+        while (p->next != h && idx <= split_count) {
+            if (cnt == spl[idx-1]) {
+                split[idx] = p;
+                idx++;
+                q->next = split[idx-1];
+                p = p->next;
+                cnt++;
+                continue;
+            }
+            cnt++;
+            q = p;
+            p = p->next;
+        }
+        if (cnt == spl[idx-1]) {
+            split[idx] = p;
+            idx++;
+            q->next = split[idx-1];
+            p = p->next;
+            cnt++;
+        }
+        cout << "Splitted\n";
+        cin.ignore();
+    }
+    void c_split_list_show(int cnt) {
+        for (int i{0}; i < cnt; i++) {
+            p = split[i];
+            if (p->next == h) {
+                cout << "List empty" << endl;
+            }
+            while (p->next != h) {
+                cout << " -> " << p->data;
+                p = p->next;
+            }
+            cout << " -> " << p->data;
+            cout << endl;
+        }
+        cin.ignore();
+    }
+    void c_merge(int cnt) {
+        for (int i{0}; i < split_count; i++) {
+            p = split[i];
+            if (p == NULL) {
+                cout << "List empty" << endl;
+            }
+            while (p->next != split[i]) {
+                p = p->next;
+            }
+            p->next = split[i+1];
+            split[i+1] = NULL;
+            split_count--;
+        }
+        split[0] = h;
+        cout << "Merged\n";
+        cin.ignore();
+    }
+    void c_search() {
+        c_display_Data();
+        p = h;
+        cout << "\nWhich number to search ? ";
+        int pos_val;
+        cin >> pos_val;
+        int cnt{1};
+        system("cls");
+        bool fg{1}, repeat{0};
+        cout << "Want to search all the ? (1 or 0) : ";
+        cin >> repeat;
+        while (p->next != h) {
+            if (p->data == pos_val) {
+                cout << "\nFound\nAt : " << cnt << " node\n";
+                cin.ignore();
+                cin.ignore();
+                fg = 0;
+                if (!repeat) return;
+            }
+            p = p->next;
+            cnt++;      // Keeping track of how many nodes have been visited
+        }
+        if (fg) cout << "Not Found\n";
+        cin.ignore();
+        cin.ignore();
+    }
+    void c_update_Data() {
+        c_display_Data();
+        p = h;
+        cout << "\nWhich number to update ? ";
+        int pos_val;
+        cin >> pos_val;
+        cout << "Enter new number : ";
+        int up_val;
+        cin >> up_val;
+        system("cls");
+        bool fg{1}, repeat{0};
+        cout << "Want to search all the ? (1 or 0) : ";
+        cin >> repeat;
+        while (p->next != h) {
+            if (p->data == pos_val) {
+                p->data = up_val;
+                cout << "\n\tData Updated\n";
+                cin.ignore();
+                cin.ignore();
+                fg = 0;
+                if (!repeat) return;
+            }
+            p = p->next;
+        }
+        if (fg) cout << "Val not found\n";
+        cin.ignore();
+        cin.ignore();
+    }
+
+/* End of functions */
 };
 
 int main() {
     linked_list l;
-    // linked_list list[100];
-
+    int cir{2};
+    cout << "1) Circular Linked List\n";
+    cout << "2) Liner Linked List\n";
+    cin >> cir;
     while (1) {
         system("cls");
         cout << "*****MENU*****" <<endl;
@@ -281,48 +568,57 @@ int main() {
         switch (option) {
             case 1:
                 system("cls");
-                l.create();
+                if (cir == 2) l.create();
+                else l.c_create();
                 break;
             case 2:
                 system("cls");
-                l.insert_Data();
+                if (cir == 2) l.insert_Data();
+                else l.c_insert_Data();
                 cout << endl;
                 break;
             case 3:
                 system("cls");
-                l.delete_Data();
+                if (cir == 2) l.delete_Data();
+                else l.c_delete_Data();
                 cout << endl;
                 break;
             case 4:
                 system("cls");
-                l.update_Data();
+                if (cir == 2) l.update_Data();
+                else l.c_update_Data();
                 cout << endl;
                 break;
             case 5:
                 system("cls");
-                l.display_Data();
+                if (cir == 2) l.display_Data();
+                else l.c_display_Data();
                 cout << endl;
                 cin.ignore();
                 break;
             case 6:
                 system("cls");
-                l.search();
+                if (cir == 2) l.search();
+                else l.c_search();
                 cout << endl;
                 break;
             case 7:
-                l.split_list();
+                if (cir == 2) l.c_split_list();
+                else l.c_split_list();
                 break;
             case 8:
                 cout << "How many list to merge ?" << endl;
                 cout << "Available is " << l.s_cnt() << endl;
                 cin >> cnt;
-                l.merge(cnt);
+                if (cir == 2) l.merge(cnt);
+                else l.c_merge(cnt);
                 break;
             case 9:
                 cout << "How many list to show ?" << endl;
                 cout << "Available is " << l.s_cnt()+1 << endl;
                 cin >> cnt;
-                l.split_list_show(cnt);
+                if (cir == 2) l.split_list_show(cnt);
+                else l.c_split_list_show(cnt);
                 cin.ignore();
                 break;
         }
